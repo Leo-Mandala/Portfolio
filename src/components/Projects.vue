47f06947f122206504942860c3902175357db702
leo-mandala <leo.mandala@outlook.fr>
2023-11-14 14:19:13 +0100

    [+] add project links

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -7,27 +7,27 @@
           <p>Mon portfolio personnel présentant mes compétences et projets (ce que vous etes entrain de regarder).</p>
         </div>
         <div class="bg-gray-800 p-4 rounded-md">
-          <h3 class="text-lg font-semibold mb-2">Application de Gestion de Tâches</h3>
-          <p>Une application web simple pour gérer vos tâches quotidiennes.</p>
+          <a class="text-lg font-semibold mb-2" href="https://leo-mandala.github.io/ToDos/">Taches a feur</a>
+          <p>Une application web simple pour gérer vos tâches quotidiennes.</p>https://leo-mandala.github.io/ToDos/
         </div>
         <div class="bg-gray-800 p-4 rounded-md">
-          <h3 class="text-lg font-semibold mb-2">Blog Personnel</h3>
+          <a class="text-lg font-semibold mb-2" href="https://leo-mandala.github.io/Cooking/">Blog Personnel</a>
           <p>Un blog où je partage mes idées et expériences= culinaires (plus ou moins réussi).</p>
         </div>
         <div class="bg-gray-800 p-4 rounded-md">
-          <h3 class="text-lg font-semibold mb-2">Application de visualisation d'anime</h3>
+          <a class="text-lg font-semibold mb-2" href="https://leo-mandala.github.io/Anime/">Application de recherche anime</a>
           <p>un site qui permet de chercher et de verifier si son animé préféré est disponible sur chrunchyroll.</p>
         </div>
         <div class="bg-gray-800 p-4 rounded-md">
-          <h3 class="text-lg font-semibold mb-2">Git Search</h3>
+          <a class="text-lg font-semibold mb-2" href="https://leo-mandala.github.io/Search/">Git Search</a>
           <p>Application permettant de chercher et regarder les repositories des utilisateur de Github.</p>
         </div>
         <div class="bg-gray-800 p-4 rounded-md">
-          <h3 class="text-lg font-semibold mb-2">Chat App</h3>
+          <a class="text-lg font-semibold mb-2" href="https://leo-mandala.github.io/ChatApp/">Chat App</a>
           <p>Application de chat simple permettant de discuter en petit commité</p>
         </div>
         <div class="bg-gray-800 p-4 rounded-md">
-          <h3 class="text-lg font-semibold mb-2">Weather App</h3>
+          <a class="text-lg font-semibold mb-2" href="https://leo-mandala.github.io/WeatherReport/">Weather App</a>
           <p>Pour savoir le temps qu'il fait actuellement ou vous vivez (ou ailleurs)</p>
         </div>
       </div>
